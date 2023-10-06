d590a770a6434a7c458ec81096550ed39b62f39c
matmoncon <mat@moncon.at>
2023-10-06 11:39:24 +0200

    fix: Fix docker build failing because of missing graph plugin

--- tools/docker/prune_monorepo.py
+++ tools/docker/prune_monorepo.py
@@ -1,16 +1,15 @@
 """
-Python script for generating a pruned version of the monorepo for use in Docker builds.
-
-This script will resolve the dependencies of the application being build and only copy them
-into a 'out' directory. This directory can then be used as the build context for a Docker build.
+Used to prune inputs to the Dockerfile to only what is needed for the build.
 """
+import argparse
 import json
+import logging
 import os
 import shutil
 import sys
 from typing import List, TypedDict
 
-from logger import get_logger
+logging.basicConfig(level=logging.INFO)
 
 
 class ProjectConfiguration(TypedDict):
@@ -22,94 +21,91 @@ class ProjectConfiguration(TypedDict):
     implicitDependencies: List[str]
 
 
-logger = get_logger("prune_monorepo")
+parser = argparse.ArgumentParser()
+parser.add_argument("--name", help="Name of the service the Dockerfile belongs to")
 
-# Read JSON from stdin produces by NX
+args = parser.parse_args()
+
+
 project_configuration: ProjectConfiguration
-service = os.environ.get("NX_PYTHON_POETRY_SERVICE", None)
-ignore_pattern = shutil.ignore_patterns("node_modules", "out", "dist", "venv", ".venv")
+implicit_dependencies: List[str] = []
+service_name = args.name
+out_dir_name = "out"
+ignore_pattern = shutil.ignore_patterns("node_modules", out_dir_name, "dist", "venv", ".venv")
 build_files = [
-    ".gitignore",
     "package.json",
     "pnpm-lock.yaml",
     "nx.json",
     "tsconfig.base.json",
 ]
 
-if service is None:
-    logger.error("Could not find 'NX_PYTHON_POETRY_SERVICE' environment variable")
+logging.info("[DOCKER] Checking for `--name` argument")
+if service_name is None:
+    logging.error("[CRITICAL] Missing `--name` argument")
     sys.exit(1)
 
-logger.info("Reading project configuration from project.json file")
-with open(os.path.join("services", service, "project.json"), "r", encoding="utf-8") as project_file:
+# Get `project.json` file from service. This file contains the dependencies to
+# libraries in the workspace and can be used to determine what has to be copied
+# into the Docker image.
+logging.info("[DOCKER] Found service %s, getting project configuration", service_name)
+with open(os.path.join("services", service_name, "project.json"), "r", encoding="utf-8") as project_file:
     project_configuration = json.load(project_file)
 
-implicit_dependencies = project_configuration.get("implicitDependencies", [])
-
-if not os.path.exists("out"):
-    logger.debug("Creating out directory")
-    os.mkdir("out")
+if "implicitDependencies" in project_configuration:
+    implicit_dependencies = [*project_configuration["implicitDependencies"], "nx-python"]
 else:
-    logger.debug("Clearing out directory")
-    shutil.rmtree("out")
-    os.mkdir("out")
+    implicit_dependencies = ["nx-python"]
+
+# Create a `out` directory, which contains only the files needed for the build.
+if not os.path.exists(out_dir_name):
+    logging.info("[DOCKER] Creating out directory")
+    os.mkdir(out_dir_name)
+else:
+    logging.warning("[DOCKER] Out directory already exists, pruning may be incomplete")
 
 
-logger.info("Copying implicit dependencies into out directory")
+# Copy over implicit dependencies and the service itself
+# We also have to copy over the nx-python plugin since it will do the building for us
+logging.info("[DOCKER] Copying implicit dependencies into out directory")
 for dependency in implicit_dependencies:
-    logger.debug("Building paths for dependency %s", dependency)
     source_dir = os.path.join("libs", dependency)
-    out_dir = os.path.join("out", "libs", dependency)
+    out_dir = os.path.join(out_dir_name, "libs", dependency)
 
     if not os.path.exists(source_dir):
-        logger.error("Could not find dependency %s", dependency)
+        logging.error("[CRITICAL] Could not find dependency %s", dependency)
         sys.exit(1)
 
     if not os.path.isdir(source_dir):
-        logger.error("Dependency %s is not a directory", dependency)
+        logging.error("[CRITICAL] Dependency %s is not a directory", dependency)
         sys.exit(1)
 
-    logger.debug("Copying %s to %s", source_dir, out_dir)
+    logging.info("[DOCKER] Copying implicit dependency %s into %s", source_dir, out_dir)
     shutil.copytree(source_dir, out_dir, ignore=ignore_pattern)
 
 
-logger.info("Copying application into out directory")
 source_dir = os.path.join("services", project_configuration["name"])
-out_dir = os.path.join("out", "services", project_configuration["name"])
+out_dir = os.path.join(out_dir_name, "services", project_configuration["name"])
+logging.info("[DOCKER] Copying service %s into %s", service_name, out_dir)
 
 if not os.path.exists(source_dir):
-    logger.error("Could not find application %s", project_configuration["name"])
+    logging.error("[CRITICAL] Could not find application %s", project_configuration["name"])
     sys.exit(1)
 
 if not os.path.isdir(source_dir):
-    logger.error("Application %s is not a directory", project_configuration["name"])
+    logging.error("[CRITICAL] Application %s is not a directory", project_configuration["name"])
     sys.exit(1)
 
-logger.debug("Copying %s to %s", source_dir, out_dir)
+logging.info("[DOCKER] Copying %s to %s", source_dir, out_dir)
 shutil.copytree(source_dir, out_dir, ignore=ignore_pattern)
 
 
-logger.info("Copying dependency graph plugin into out directory")
-source_dir = os.path.join("tools", "plugins")
-out_dir = os.path.join("out", "tools", "plugins")
-
-if not os.path.exists(source_dir):
-    logger.error("Could not find dependency graph plugin")
-    sys.exit(1)
-
-if not os.path.isdir(source_dir):
-    logger.error("Dependency graph plugin is not a directory")
-    sys.exit(1)
-
-logger.debug("Copying %s to %s", source_dir, out_dir)
-shutil.copytree(source_dir, out_dir, ignore=ignore_pattern)
-
-
-logger.info("Copying build files into out directory")
+logging.info(
+    "[DOCKER] Copying workspace package.json, pnpm-lock.yaml, tsconfig.base.json and nx.json into %s directory", out_dir
+)
 for build_file in build_files:
-    out_path = os.path.join("out", build_file)
+    out_path = os.path.join(out_dir_name, build_file)
 
-    logger.debug("Copying %s to %s", build_file, out_path)
+    logging.info("[DOCKER] Copying %s to %s", build_file, out_path)
     shutil.copy(build_file, out_path)
 
-logger.info("Pruning complete")
+logging.info("[DOCKER] Pruning complete")
